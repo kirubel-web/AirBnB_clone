88751bc257f79fdb51459d817b6b65ca8cb2a9c3
kirubel-web <kirubelalemu119@gmail.com>
2024-01-15 00:03:21 +0300

    some fix

--- console.py
+++ console.py
@@ -4,13 +4,14 @@ import cmd
 import re
 from shlex import split
 from models import storage
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
 from models.review import Review
+from models.state import State
+from models.user import User
+
 
 
 def parse(arg):
